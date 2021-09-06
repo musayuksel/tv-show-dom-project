ac484dabd3aa761cf48fa366f0bc4e8cbf2f18a9
musayuksel <musayuxel@gmail.com>
2021-09-06 15:43:30 +0100

    feat: add season number format

--- createEpisodeContainer.js
+++ createEpisodeContainer.js
@@ -4,22 +4,20 @@ export default function createEpisodeContainer({name,season,number,image,summary
   const card = document.createElement('article');
   card.className="card";
 
-  //name
+  //name, season & episode number format
   const h2 = document.createElement('h2');
-  h2.innerText=name;
-  //season & number
-  const p = document.createElement('p');
-  p.innerText = `${season} ${number}`
+  h2.className = "cardHeading"
+  h2.innerText=`${name} - S${(''+season).padStart(2,0)}E${(''+number).padStart(2,0)}`;
+
   //episode image
   const img = document.createElement('img')
   img.src = `${image.medium}`;
-
+  //summary
   const summaryContainer = document.createElement('article');
   summaryContainer.innerHTML=summary;//summary is html tag
 
-  card.appendChild(h2)
-  card.appendChild(p)
-  card.appendChild(img)
-  card.appendChild(summaryContainer)
+  card.appendChild(h2);
+  card.appendChild(img);
+  card.appendChild(summaryContainer);
   return card;
 }
