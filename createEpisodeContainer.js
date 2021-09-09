06d9a39438d6d715d4d71b20365bb8457102fe88
musayuksel <musayuxel@gmail.com>
2021-09-09 08:13:18 +0100

    style: add sCSS styles

--- createEpisodeContainer.js
+++ createEpisodeContainer.js
@@ -1,7 +1,13 @@
-
-// create each episode card
-export default function createEpisodeContainer({name,season,number,image,summary}){
-  const card = document.createElement('article');
+// create episode card for each episode
+export default function createEpisodeContainer(
+  {name,
+  season,
+  number,
+  image,
+  summary,
+  runtime,
+  url}){
+  const card = document.createElement('li');
   card.className="card";
 
   //name, season & episode number format
@@ -12,12 +18,50 @@ export default function createEpisodeContainer({name,season,number,image,summary
   //episode image
   const img = document.createElement('img')
   img.src = `${image.medium}`;
+  img.alt= name;
+  img.className = 'episodeImg';
+
   //summary
   const summaryContainer = document.createElement('article');
   summaryContainer.innerHTML=summary;//summary is html tag
+  const icon = document.createElement('img');
+  icon.src = '/icons/arrow.png';
+  icon.alt = "arrow"
+  icon.className = 'arrowIcon'
+  //summary click event
+  icon.addEventListener('click', ()=>{
+    summaryContainer.classList.toggle("openSummary");
+  })
+  summaryContainer.appendChild(icon);
+
+  //Run Time
+  const span = document.createElement('span');
+  span.innerText = formatedRunTime(runtime);// format the time
+  summaryContainer.appendChild(span)
+
+  //Play button
+  const movieLink =document.createElement('a');
+  movieLink.href =url;
+  movieLink.target ='_blank';
+  movieLink.className = 'episodeUrl'
+  const play = document.createElement('img');
+  play.src = '/icons/play.png';
+  play.alt = "play icon";
+  play.className = 'playIcon';
+  movieLink.appendChild(play);
+  summaryContainer.appendChild(movieLink);
 
   card.appendChild(h2);
   card.appendChild(img);
   card.appendChild(summaryContainer);
   return card;
+}
+
+
+function formatedRunTime(time){
+  // take time as a min, convert it to hour format (65 => 1h 05min)
+  const min = ((time % 60)+'').padStart(2,0);
+  const hour = Math.floor(time / 60);
+  return `${hour}h ${min}min`;
+
 }
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 import createEpisodeContainer from './createEpisodeContainer.js'
 import searchEpisode from './searchEpisode.js'
 import  episodeSelectMenu from './selectMenu.js'
+
 //take Episodes from DB
 let allEpisodes = [];// declared global because i will use with other func.
 function setup() {
@@ -12,9 +13,9 @@ function setup() {
 //prepare the page
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.innerHTML ='';
+  rootElem.innerHTML ='';//this way is NOT effective for DOM !!!
 
-  const main =document.createElement('main'); //main container
+  const main = document.createElement('ul'); //main container
   main.classList.add('mainContainer');
 
   //for every episode create cards
@@ -25,24 +26,19 @@ function makePageForEpisodes(episodeList) {
   rootElem.appendChild(main);
 }
 
-
-
-
-setup()
-// window.onload = setup;
+window.onload = setup;
 
 //Search function
 const seachArea = document.getElementById('search');
-const serachLabel = document.getElementById('serachLabel');
+const searchLabel = document.getElementById('searchLabel');
 seachArea.addEventListener('input', (event)=>{
     event.preventDefault();
     const episodesIncludeInput= searchEpisode(allEpisodes ,seachArea.value);
-    serachLabel.innerText = `Displaying ${episodesIncludeInput.length}/${allEpisodes.length} episodes`;
+    seachArea.value === '' ? searchLabel.innerText ='':
+    searchLabel.innerText = `Displaying ${episodesIncludeInput.length}/${allEpisodes.length} episodes`;
     makePageForEpisodes(episodesIncludeInput);
     episodeSelectMenu(episodesIncludeInput);//update select menu
   });
-
-
 
 //Episode select menu
 const select = document.getElementById('episodeSelect');
