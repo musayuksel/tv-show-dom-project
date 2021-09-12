e2f9af5a9ad74dd3c7c5d9d8860b9eb470fb6a2f
musayuksel <musayuxel@gmail.com>
2021-09-12 15:06:13 +0100

    feat: add Show select menu event

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
 import createEpisodeContainer from './Episodes/createEpisodeContainer.js'
 import searchEpisode from './Episodes/searchEpisode.js'
-import  episodeSelectMenu from './Episodes/selectMenu.js'
+import  episodeSelectMenu from './Episodes/EpisodeSelectMenu.js'
 import deleteElementsWithCss from './Episodes/deleteElementsWithCss.js'
 import showSelectMenu from './Shows/showSelectMenu.js'
 import getAllShows from './Shows/shows.js'
 //take Episodes from DB
 let allEpisodes = [];// declared global because i will use with other func.
-let url ='https://api.tvmaze.com/shows/83/episodes'
-//https://api.tvmaze.com/shows/82/episodes
-fetch(url)
-  .then(response=>response.json())
-  .then(data =>{
-    allEpisodes = data;
-    console.log(data);
-     setup()
-    }
-);
+let url =''
+function fetchData (url ='https://api.tvmaze.com/shows/5/episodes'){
+  fetch(url)
+    .then(response=>response.json())
+    .then(data =>{
+      allEpisodes = data;
+       setup()
+      }
+  );
+}
 
 function setup() {
   // allEpisodes = getAllEpisodes();
   episodeSelectMenu(allEpisodes)
   makePageForEpisodes(allEpisodes);
-  const showsArray =getAllShows()
-  showSelectMenu(showsArray);
  
 }
 
@@ -42,8 +40,12 @@ function makePageForEpisodes(episodeList) {
   rootElem.appendChild(main);
 }
 
-// window.onload = setup;
-
+window.onload = ()=>{
+fetchData();
+//select menu will work one time
+const showsArray =getAllShows();
+showSelectMenu(showsArray);
+}
 //SEARCH FUNCTION
 const seachArea = document.getElementById('search');
 const searchLabel = document.getElementById('searchLabel');
@@ -63,7 +65,13 @@ seachArea.addEventListener('input', (event)=>{
   select.addEventListener('change',(event)=>{
     const selectedEpisode = searchEpisode(allEpisodes, event.target.value);
     // makePageForEpisodes(selectedEpisode);
+
     deleteElementsWithCss(selectedEpisode);
 });
-
+  //SHOW SELECT MENU
+  const showSelect = document.getElementById('showSelect');
+  showSelect.addEventListener('change',(event)=>{
+    url =`https://api.tvmaze.com/shows/${event.target.value}/episodes`
+    fetchData(url)
+});
 
