3b21ea6b1e39768b93001334b0670d6d07827e36
musayuksel <musayuxel@gmail.com>
2021-09-13 04:17:49 +0100

    feat:add sort function for show array

---
+++ Shows/sortArrayAlphabeticalOrder.js
@@ -0,0 +1,9 @@
+export default function alphabeticalOrder(showArray){
+  return showArray.sort((a,b) => {
+    const name1 =  a.name.toLowerCase()
+    const name2 =  b.name.toLowerCase();
+    if(name1 < name2){ return -1}
+    if(name1 > name2){ return 1}
+    return 0; 
+  });
+}
