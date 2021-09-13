c1be6c35ff5b3d4d91364eb8e0e21a0052bd486a
musayuksel <musayuxel@gmail.com>
2021-09-13 04:19:34 +0100

    feat:add show component and main script functions

---
+++ Shows/showComponent.js
@@ -0,0 +1,65 @@
+export default function showComponent(
+{
+  name,
+  rating,
+  summary,
+  genres,
+  status,
+  runtime,
+  image,
+  id
+}) {
+  //, , status, , and runtime
+  const showCard = document.createElement('li');
+  showCard.className ='showCard'//daha sonra degistir
+  showCard.id = id;
+
+  const h2 = document.createElement('h2');
+  h2.className = "showHeading"
+  h2.innerText = name;
+
+  const img = document.createElement('img');
+  img.className = 'showImg';
+  img.src = `${image !== null ? image.medium: ''}`;
+  img.alt = name;
+
+  const showContainer = document.createElement('article');
+  showContainer.innerHTML=summary;//summary is html tag
+
+  const statusContainer = document.createElement('article');
+  const ratedP = document.createElement('p');
+  ratedP.innerText = 'Rated: ';
+  const ratedSpan = document.createElement('span');
+  ratedSpan.innerText = rating.average;
+  ratedP.appendChild(ratedSpan);
+  statusContainer.appendChild(ratedP);
+
+  const genresP = document.createElement('p');
+  genresP.innerText = 'Genres: ';
+  const genresSpan = document.createElement('span');
+  genresSpan.innerText = genres.join(' | ');
+  genresP.appendChild(genresSpan);
+  statusContainer.appendChild(genresP);
+
+  const statusP = document.createElement('p');
+  statusP.innerText = 'Status: ';
+  const statusSpan = document.createElement('span');
+  statusSpan.innerText = status;
+  statusP.appendChild(statusSpan);
+  statusContainer.appendChild(statusP);
+
+  const runtimeP = document.createElement('p');
+  runtimeP.innerText = 'Runtime: ';
+  const runtimeSpan = document.createElement('span');
+  runtimeSpan.innerText = runtime;
+  runtimeP.appendChild(runtimeSpan);
+  statusContainer.appendChild(runtimeP);
+
+
+  showCard.appendChild(h2)
+  showCard.appendChild(img)
+  showCard.appendChild(showContainer)
+  showCard.appendChild(statusContainer)
+  return showCard;
+
+}
--- script.js
+++ script.js
@@ -3,10 +3,22 @@ import searchEpisode from './Episodes/searchEpisode.js'
 import  episodeSelectMenu from './Episodes/EpisodeSelectMenu.js'
 import deleteElementsWithCss from './Episodes/deleteElementsWithCss.js'
 import showSelectMenu from './Shows/showSelectMenu.js'
-import getAllShows from './Shows/shows.js'
+import showComponent from './Shows/showComponent.js';
+import searchShow from './Shows/searchShow.js';
+import alphabeticalOrder from './Shows/sortArrayAlphabeticalOrder.js'
 //take Episodes from DB
 let allEpisodes = [];// declared global because i will use with other func.
 let url =''
+let showsArray  = [];
+
+function getShows(){
+  //getAllShows funct will work one time
+  showsArray =getAllShows();
+  alphabeticalOrder(showsArray);
+  makePageForShows(showsArray)
+  showSelectMenu(showsArray);
+}
+
 function fetchData (url ='https://api.tvmaze.com/shows/5/episodes'){
   fetch(url)
     .then(response=>response.json())
@@ -18,20 +30,44 @@ function fetchData (url ='https://api.tvmaze.com/shows/5/episodes'){
 }
 
 function setup() {
-  // allEpisodes = getAllEpisodes();
+  
   episodeSelectMenu(allEpisodes)
   makePageForEpisodes(allEpisodes);
+  
  
 }
 
-//PREPARE THE PAGE
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.innerHTML ='';//this way is NOT effective for DOM !!!
+
+
+const rootElem = document.getElementById("root");//i will use for 2 function
+//PREPARE THE PAGE FOR SHOWS
+function makePageForShows(showList){
+  document.getElementById('showsMenuContainer').style.display = 'block';
+  document.getElementById('episodesMenuContainer').style.display = 'none';
+  
+
 
   const main = document.createElement('ul'); //main container
   main.classList.add('mainContainer');
-
+  rootElem.innerHTML ='';
+  showList.forEach(show =>{
+    const showCard = showComponent(show);
+    showCard.addEventListener('click', ()=>{
+      url =`https://api.tvmaze.com/shows/${show.id}/episodes`
+      fetchData(url)
+    })
+    main.appendChild(showCard);
+  })
+  rootElem.appendChild(main);
+}
+//PREPARE THE PAGE FOR EPISODES
+function makePageForEpisodes(episodeList) {
+  document.getElementById('showsMenuContainer').style.display = 'none';
+  document.getElementById('episodesMenuContainer').style.display = 'flex';
+  
+  const main = document.createElement('ul'); //main container
+  main.classList.add('mainContainer');
+  rootElem.innerHTML = '';
   //for every episode create cards
   episodeList.forEach(episode =>{
     const allCard=createEpisodeContainer(episode);
@@ -40,38 +76,44 @@ function makePageForEpisodes(episodeList) {
   rootElem.appendChild(main);
 }
 
-window.onload = ()=>{
-fetchData();
-//select menu will work one time
-const showsArray =getAllShows();
-showSelectMenu(showsArray);
-}
+
 //SEARCH FUNCTION
 const seachArea = document.getElementById('search');
 const searchLabel = document.getElementById('searchLabel');
 seachArea.addEventListener('input', (event)=>{
-    event.preventDefault();
-    const episodesIncludeInput= searchEpisode(allEpisodes ,seachArea.value);
-    seachArea.value === '' ? searchLabel.innerText ='':
-    searchLabel.innerText = `Displaying ${episodesIncludeInput.length}/${allEpisodes.length} episodes`;
-    
-    deleteElementsWithCss(episodesIncludeInput)
-    // makePageForEpisodes(episodesIncludeInput);
-    episodeSelectMenu(episodesIncludeInput);//update select menu
-  });
+  event.preventDefault();
+  const episodesIncludeInput= searchEpisode(allEpisodes ,seachArea.value);
+  seachArea.value === '' ? searchLabel.innerText ='':
+  searchLabel.innerText = `Displaying ${episodesIncludeInput.length}/${allEpisodes.length} episodes`;
   
-  //EPISODE SELECT MENU
-  const select = document.getElementById('episodeSelect');
-  select.addEventListener('change',(event)=>{
-    const selectedEpisode = searchEpisode(allEpisodes, event.target.value);
-    // makePageForEpisodes(selectedEpisode);
-
-    deleteElementsWithCss(selectedEpisode);
-});
-  //SHOW SELECT MENU
-  const showSelect = document.getElementById('showSelect');
-  showSelect.addEventListener('change',(event)=>{
-    url =`https://api.tvmaze.com/shows/${event.target.value}/episodes`
-    fetchData(url)
+  deleteElementsWithCss(episodesIncludeInput)
+  episodeSelectMenu(episodesIncludeInput);//update select menu
 });
 
+//EPISODE SELECT MENU
+const select = document.getElementById('episodeSelect');
+select.addEventListener('change',(event)=>{
+  const selectedEpisode = searchEpisode(allEpisodes, event.target.value);
+  deleteElementsWithCss(selectedEpisode);
+});
+//SHOW SELECT MENU
+const showSelect = document.getElementById('showSelect');
+showSelect.addEventListener('change',(event)=>{
+  url =`https://api.tvmaze.com/shows/${event.target.value}/episodes`
+  fetchData(url)
+});
+
+//SHOW SEARCH MENU
+const  showSearch = document.getElementById('showSearch');
+const  showSearchLabel = document.getElementById('showSearchLabel');
+showSearch.addEventListener('input',(event)=>{
+  event.preventDefault();
+  const showsIncludeInput = searchShow(showsArray,event.target.value )
+  deleteElementsWithCss(showsIncludeInput)
+  showSelectMenu(showsIncludeInput)
+  event.target.value === '' ? showSearchLabel.innerText ='':
+  showSearchLabel.innerText = `found ${showsIncludeInput.length} shows`;
+})
+
+
+window.onload = getShows;
